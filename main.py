d1589cf620ab992ec7593ee5048856ce875482e9
tuyfyycr793-creator <tuyfyycr793@gmail.com>
2025-12-01 23:44:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ from flask import Flask
 import threading
 import telebot
 
-TOKEN = "8156062603:AAGPtV0nhKmziDO9KvUjBsvAwk9VExe9Ljc"
+TOKEN = "TU_TOKEN_AQUI"
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
+# Ruta web mínima para que Render mantenga activo el servicio
 @app.route("/")
 def home():
     return "Bot activo ✅"
 
+# Función para correr Flask en un hilo aparte
 def run_webserver():
     app.run(host="0.0.0.0", port=3000)
 
+# Inicia Flask en un hilo
 threading.Thread(target=run_webserver).start()
+
+# Inicia el bot de Telegram
 bot.infinity_polling()
